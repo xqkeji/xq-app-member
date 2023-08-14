6763011da592cb73985a3c83f47f2c750b11dadb
xqkeji <xqkeji@foxmail.com>
2023-08-14 19:53:39 +0800

    更新翻译文件

--- src/lang/zh-cn.php
+++ src/lang/zh-cn.php
@@ -1,10 +1,41 @@
 <?php
 return [
-	'member-user-admin'=>'用户管理',
-	'member-group-admin'=>'用户组管理',
-	'member-group-add'=>'添加用户组',
-	'member-group-edit'=>'编辑用户组',
-	'add group success'=>'添加用户组成功',
-	'edit group success'=>'添加用户组成功',
-	'delete group success'=>'删除用户组成功',
+	'member module auth'=>'会员管理',
+    //用户页面
+	'member user admin title'=>'用户管理',
+    'member user add title'=>'添加用户',
+    'member user edit title'=>'编辑用户',
+    'member user add success'=>'添加用户成功',
+    'member user add failed'=>'添加用户失败',
+    'member user edit success'=>'编辑用户成功',
+    'member user edit failed'=>'编辑用户失败',
+    'member user delete success'=>'删除用户成功',
+    'member user delete failed'=>'删除用户失败',
+    //用户权限
+    'member module user auth'=>'用户管理',
+    'member module user admin auth'=>'用户管理',
+    'member module user add auth'=>'添加用户',
+    'member module user edit auth'=>'编辑用户',
+    'member module user delete auth'=>'删除用户',
+    'member module user change auth'=>'修改用户状态',
+    'member module user b_delete auth'=>'批量删除用户',
+
+	//用户组页面
+	'member group admin title'=>'用户组管理',
+	'member group add title'=>'添加用户组',
+	'member group edit title'=>'编辑用户组',
+	'member group add success'=>'添加用户组成功',
+	'member group add failed'=>'添加用户组失败',
+	'member group edit success'=>'编辑用户组成功',
+	'member group edit failed'=>'编辑用户组失败',
+	'member group delete success'=>'删除用户组成功',
+	'member group delete failed'=>'删除用户组失败',
+	//用户组权限
+	'member module group auth'=>'用户组管理',
+	'member module group admin auth'=>'用户组管理',
+	'member module group add auth'=>'添加用户组',
+	'member module group edit auth'=>'编辑用户组',
+	'member module group delete auth'=>'删除用户组',
+	'member module group change auth'=>'修改用户组状态',
+	'member module group b_delete auth'=>'批量删除用户组',
 ];
