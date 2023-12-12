a364e4f0fd66dd0758ed11a9f3aabed9c0c27ce5
xqkeji <xqkeji@foxmail.com>
2023-12-12 19:44:27 +0800

    更新菜单格式

--- src/config/menu.php
+++ src/config/menu.php
@@ -1,19 +1,22 @@
 <?php
-return [	
-	'title'=>'会员管理',
-	'children'=>[
-		[
-			'url'=>'user/admin',
-			'title'=>'用户管理',
-			'icon'=>'bi bi-person-lines-fill',
+return [
+	'admin'=>[
+		'title'=>'会员管理',
+		'children'=>[
+			[
+				'url'=>'user/admin',
+				'title'=>'用户管理',
+				'icon'=>'bi bi-person-lines-fill',
+			],
+			[
+				'url'=>'group/admin',
+				'title'=>'用户组管理',
+				'icon'=>'bi bi-person-hearts',
+			],
+			
+			
 		],
-		[
-			'url'=>'group/admin',
-			'title'=>'用户组管理',
-			'icon'=>'bi bi-person-hearts',
-		],
-		
-		
-	],
+	]
+	
 	
 ];
